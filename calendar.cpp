e05032149e9ed5d27719805114c4009c28e6470e
kynkaigg <tominagy2003@gmail.com>
2025-04-08 11:12:25 +0200

    practically emptied

--- calendar.cpp
+++ calendar.cpp
@@ -3,19 +3,20 @@
 #include "calendar.h"
 #include "event.h"
 #include "string.h"
-#include "datetime.h"
+#include "date.h"
+#include "time.h"
 #include <algorithm>
 
 /* Konstruktorok */
 /*
-Calendar::Calendar(const Calendar& rhs) :nEvents(rhs.nEvents), events(new Event[nEvents]) {
+EventStore::EventStore(const EventStore& rhs) :nEvents(rhs.nEvents), events(new Event[nEvents]) {
     size_t i = 0;
     for (Event &e : *this) {
         e = rhs.events[i++];
     }
 }
 
-Calendar& Calendar::operator=(const Calendar& rhs) {
+EventStore& EventStore::operator=(const EventStore& rhs) {
     if (this != &rhs) {
         delete[] events;
         nEvents = rhs.nEvents;
@@ -29,11 +30,7 @@ Calendar& Calendar::operator=(const Calendar& rhs) {
 }
     */
 
-void Calendar::sort() {
+void EventStore::sort() {
     //std::sort(this->begin(), this->end());
 }
 
-std::ostream& operator<<(std::ostream& os, const Event& rhs){
-    os << rhs.getEvTime() << ' ' <<rhs.getEvDesc();
-    return os;
-}
